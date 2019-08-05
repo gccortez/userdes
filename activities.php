f0b903a33f1e965955c3f2058821d9d9855e7942
Gian Cortez <52141626+gccortez@users.noreply.github.com>
2019-08-05 21:09:31 +0800

    Update activities.php

--- activities.php
+++ activities.php
@@ -17,25 +17,59 @@
 
   <link type="text/css"  rel="stylesheet"  href="css&script/styles.css">
 
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+  
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+  
+  crossorigin="anonymous">
 </head>
 
 <body>
 
-  <div id="navbar">
+<!--  <div id="navbar">
 
-    <a href="reflections.php">
+    <a href="reflections.html">
 
       Reflections
 
     </a>
 
-    <a href="index.php">
+    <a href="index.html">
 
       Home
 
     </a>
   
   </div>
+
+-->
+
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-info">
+    <a class="navbar-brand" href="index.php">Gian.</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="activities.php">Activities</a>
+        </li>
+        <!--<li class="nav-item">
+            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+        </li>
+      -->
+        <li class="nav-item">
+            <a class="nav-link" href="reflections.php">Reflections</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+
+    </div>
+  </nav>
 
   <div id="header">
 
@@ -138,6 +172,8 @@ or her desired floor number.
 
   </div>
 
+  
+
   <div id="icons">
 
     <a href="https://www.twitter.com/gncrlcrtz" target="_blank">
@@ -176,6 +212,23 @@ or her desired floor number.
 
   <script type="text/javascript" src="css&script/script.js"></script>
 
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+  
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+  
+  crossorigin="anonymous"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+  
+  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
+  
+  crossorigin="anonymous"></script>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+  
+  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
+  
+  crossorigin="anonymous"></script>
 </body>
 
 </html>
