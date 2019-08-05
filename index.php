2e4ad9921ac50ffb9bef82476a1a74472f33148b
Gian Cortez <52141626+gccortez@users.noreply.github.com>
2019-08-05 21:10:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
 
 -->
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-info">
-  <a class="navbar-brand" href="index.html"><img src="Images/icong.png" alt="gian" width="40" height="45"></a>
+  <a class="navbar-brand" href="index.php"><img src="Images/icong.png" alt="gian" width="40" height="45"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
