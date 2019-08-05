150656c4dad61fa0128c0d413771303dbb35f6c1
Gian Cortez <52141626+gccortez@users.noreply.github.com>
2019-08-05 22:07:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -85,33 +85,33 @@
 
       <p>
 
-Hello! 
+	Hello! 
 
-Gian is an 18-year old computer science student at Asia 
-Pacific College. He lives at Villamor Air Base, Pasay City. 
-He has 2 siblings and he is the middle child. He is a 
-joyful person. Although he is not so friendly, but he is 
-nice and caring . He used to love playing computer games, 
-but he stopped because he wants to focus more on his studies.
+	Gian is an 18-year old computer science student at Asia 
+	Pacific College. He lives at Villamor Air Base, Pasay City. 
+	He has 2 siblings and he is the middle child. He is a 
+	joyful person. Although he is not so friendly, but he is 
+	nice and caring . He used to love playing computer games, 
+	but he stopped because he wants to focus more on his studies.
 
-During summer, Gian spends his free time reading the bible 
-and playing piano. He has a bad habit of studying at school, 
-last minute before a test. He procrastinates most of the time 
-and crams his school works, yet he still manages to get good 
-grades. He is not usually confident about his academic 
-performance, regardless he still aims for progression and not 
-perfection.
+	During summer, Gian spends his free time reading the bible 
+	and playing piano. He has a bad habit of studying at school, 
+	last minute before a test. He procrastinates most of the time 
+	and crams his school works, yet he still manages to get good 
+	grades. He is not usually confident about his academic 
+	performance, regardless he still aims for progression and not 
+	perfection.
 
-What makes Gian interesting?
+	What makes Gian interesting?
 
 
-Well, he resembles Grizz of We Bare Bears.
+	Well, he resembles Grizz of We Bare Bears.
 
-What Gian is expecting from this subject (USERDES) is that 
-he will learn more about designing an interface of an 
-application since it is needed for an easier interaction 
-between the user and the program. He is also expecting that 
-he will learn and have fun at the same time.
+	What Gian is expecting from this subject (USERDES) is that 
+	he will learn more about designing an interface of an 
+	application since it is needed for an easier interaction 
+	between the user and the program. He is also expecting that 
+	he will learn and have fun at the same time.
 
       </p>
 
