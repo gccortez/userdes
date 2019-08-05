f03f435c8728a9fba64b900afda640f93aa11c0f
Gian Cortez <52141626+gccortez@users.noreply.github.com>
2019-08-05 21:10:51 +0800

    Update reflections.php

--- reflections.php
+++ reflections.php
@@ -17,25 +17,58 @@
 
   <link type="text/css"  rel="stylesheet"  href="css&script/styles.css">
 
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+  
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+  
+  crossorigin="anonymous">
+
 </head>
 
 <body>
 
-  <div id="navbar">
+  <!--
+    
+    <div id="navbar">
 
-  <a href="activities.php">
+  <a href="activities.html">
 
     Activities
 
   </a>
 
-  <a href="index.php">
+  <a href="index.html">
 
     Home
 
   </a>
   
   </div>
+-->
+
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-info">
+  <a class="navbar-brand" href="index.php">Gian.</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+          <a class="nav-link" href="reflections.php">Reflections</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="activities.php">Activities</a>
+      </li>
+
+    </ul>
+    <!--<form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  -->
+  </div>
+</nav>
 
   <div id="header">
 
@@ -145,6 +178,52 @@ can have many ideas to create better project.
     
   </div>
 
+  <div id="ref3">
+
+    <h1>
+
+      REFLECTION #3
+
+    </h1>
+
+    <pre>
+
+      <p>
+
+I was into programming since when I was in Senior High School, but I think that HTML
+is not considered a programming language. It is a markup language. I don’t really know what
+that is. I thought that it will really be hard for me to create a web application since I don’t really
+have a background about it, and I am starting from 0 knowledge.
+
+I started doing it. Myself knows that my code for my web application is messy and not
+easy to understand by others. Since I do not have a background about using HTML, I searched
+for tutorials on the internet, but I am too lazy to watch tutorial videos from YouTube. I know that
+watching tutorial videos would help me a lot, but I also know that it will take me a lot of time to
+learn from it and I won’t really have much time to create my own web application. Then, I just
+read from the internet specifically the tutorials from W3School. There are codes already written
+for the beginners and it was helpful. Though I didn’t really read a lot of tutorials about what are
+the attributes, what are the syntax and even how to import CSS file to html, something like that, I
+did learn things about HTML just by looking at the codes given by W3School and testing the
+output of the code. It was fun doing my own web application and I got the chance to design my
+own website and add some features that I want. I added music since I really love music and my
+girlfriend suggested it. What I really hated about doing the web application is that I was a bit
+annoyed on positioning the elements. At first, I thought that my work is neat and was positioned
+on the correct spot. But I tried sending the files to my girlfriend and when she opened the file,
+the pictures were mixed up and the whole interface was messy. So, I did it from the start and
+fixed everything, I was frustrated but the result was worthy.
+
+In conclusion, it was fun doing my own web application since I got the freedom to put
+whatever I want on my web application and I can do anything that I want. What will it look like,
+what are the features, what can my website do and such. It was fun and at the same time
+frustrating since I don’t have knowledge about HTML. But, I learned a lot and I am looking
+forward to make my web application better.
+
+      </p>
+
+    </pre>
+
+  </div>
+
   <div id="icons">
 
     <a href="https://www.twitter.com/gncrlcrtz" target="_blank">
@@ -183,6 +262,24 @@ can have many ideas to create better project.
 
   <script type="text/javascript" src="css&script/script.js"></script>
   
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+  
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+  
+  crossorigin="anonymous"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+  
+  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
+  
+  crossorigin="anonymous"></script>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+  
+  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
+  
+  crossorigin="anonymous"></script>
+
 </body>
 
 </html>
