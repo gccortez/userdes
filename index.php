a4810cd59dd062b49446402f368529a1e7d176db
Gian Cortez <52141626+gccortez@users.noreply.github.com>
2019-08-08 15:52:58 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<meta charset="utf-8">
+
+<head>
+
+  <title>Gian</title>
+
+  <link rel="shortcut icon" type="image/png" href="Images/icong.png"/>
+
+  <link type="text/css"  rel="stylesheet"  href="css&script/styles.css">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+  
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+  
+  crossorigin="anonymous">
+
+</head>
+
+<body>
+
+  <!-- 
+    
+    <div id="navbar">
+
+    <a href="activities.html">
+
+      Activities
+
+    </a>
+
+    <a href="reflections.html">
+
+      Reflections
+
+    </a>
+
+  </div>
+
+-->
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-info">
+  <a class="navbar-brand" href="index.php"><img src="Images/icong.png" alt="gian" width="40" height="45"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="activities.php">Activities</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="reflections.php">Reflections</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="quiz.php">Quiz</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="lab1-cortez.herokuapp.com" target="_blank">Lab 1</a>
+      </li>
+    </ul>
+    <!--<form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  -->
+  </div>
+</nav>
+
+  <div id="header">
+
+    Gian
+
+  </div>
+
+  <div id="intro">
+
+    <h1 class="hi">
+
+      ABOUT
+
+    </h1>
+
+    <div id="gian"><img src="Images/gian.jpg" alt="gian" width="200" height="200"></div>
+
+    <pre>
+
+      <p>
+Hello! 
+
+Gian is an 18-year old computer science student at Asia 
+Pacific College. He lives at Villamor Air Base, Pasay City. 
+He has 2 siblings and he is the middle child. He is a 
+joyful person. Although he is not so friendly, but he is 
+nice and caring . He used to love playing computer games, 
+but he stopped because he wants to focus more on his studies.
+
+During summer, Gian spends his free time reading the bible 
+and playing piano. He has a bad habit of studying at school, 
+last minute before a test. He procrastinates most of the time 
+and crams his school works, yet he still manages to get good 
+grades. He is not usually confident about his academic 
+performance, regardless he still aims for progression and not 
+perfection.
+
+What makes Gian interesting?
+
+
+Well, he resembles Grizz of We Bare Bears.
+
+What Gian is expecting from this subject (USERDES) is that 
+he will learn more about designing an interface of an 
+application since it is needed for an easier interaction 
+between the user and the program. He is also expecting that 
+he will learn and have fun at the same time.
+
+      </p>
+
+    </pre>
+
+  </div>
+
+  <div id="icons">
+
+		<a href="https://www.twitter.com/gncrlcrtz" target="_blank">
+
+      <img src="Images/twitter.png" alt="twitter" width="20" height="20">
+
+    </a>
+
+		<a href="https://www.trello.com/giancortez" target="_blank">
+
+      <img src="Images/trello.png"alt="trello" width="20" height="20">
+
+    </a>
+
+		<a href="https://www.duolingo.com/GianCortez5" target="_blank">
+
+      <img src="Images/duolingo.png"alt="duolingo" width="20" height="20">
+
+    </a>
+		
+    <a href="mailto:gccortez@student.apc.edu.ph" target="_blank">
+
+      <img src="Images/outlook.png"alt="outlook" width="20" height="20">
+
+    </a>
+		
+    <a href="https://www.github.com/gccortez" target="_blank">
+
+      <img src="Images/github.png"alt="github" width="20" height="20">
+
+    </a>
+
+  </div>
+
+  <div class="parallax"></div>
+
+  <script type="text/javascript" src="css&script/script.js"></script>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+  
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+  
+  crossorigin="anonymous"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+  
+  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
+  
+  crossorigin="anonymous"></script>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+  
+  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
+  
+  crossorigin="anonymous"></script>
+
+</body>
+
+</html>
